3890abb665c33eb8d7283a45d18d4aad6ec31fa8
Andrew-Kil <kil.andrew@gmail.com>
2018-08-29 22:17:08 -0400

    solved cent-from-year and min-max functions

---
+++ find-maximum-and-minimum-values-of-a-list.js
@@ -0,0 +1,17 @@
+var min = function(list){
+    let funcName = function(a, b){
+      return a - b;
+    }
+    list.sort(funcName);
+
+    return list[0];
+}
+
+var max = function(list){
+    let funcName2 = function(a, b){
+      return b - a;
+    }
+    list.sort(funcName2);
+
+    return list[0];
+}

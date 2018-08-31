2ab71d9dd3fb5f352ad4615fd8be2e504a4ff507
Andrew-Kil <kil.andrew@gmail.com>
2018-08-31 19:25:38 -0400

    solved invert values algorithm

---
+++ invert-values.js
@@ -0,0 +1,14 @@
+function invert(array) {
+   let finalArr = [];
+
+   for (let i = 0; i < array.length; i++) {
+     if (array[i] > 0) {
+       finalArr.push(array[i] * -1);
+     } else if (array[i] < 0) {
+       finalArr.push(array[i] * -1);
+     } else if (array[i] === 0) {
+       finalArr.push(array[i]);
+     }
+   }
+   return finalArr;
+}

34924ac9d21abea1973b58af6a9c0d39eddeea32
Andrew KIl <kil.andrew@gmail.com>
2018-09-02 09:02:07 -0400

    solved fake binary algorithm

---
+++ fake-binary.js
@@ -0,0 +1,15 @@
+//Given a string of digits, you should replace any digit below 5 with '0' and any digit 5 and above with '1'. Return the resulting string.
+
+function fakeBin(x){
+  let splitX = x.split('');
+  let finalX = [];
+
+  for (let i = 0; i < splitX.length; i++) {
+    if (splitX[i] < 5) {
+      finalX.push(0);
+    } else if (splitX[i] >= 5) {
+      finalX.push(1);
+    }
+  }
+  return finalX.join('');
+}

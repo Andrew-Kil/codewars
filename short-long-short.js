8bf39ce3c259fb22e398fb0ba01e80034fb3b94e
Andrew KIl <kil.andrew@gmail.com>
2018-09-02 15:10:41 -0400

    solved short long short algorithm using if/else and string concatenation

---
+++ short-long-short.js
@@ -0,0 +1,8 @@
+// Given 2 strings, a and b, return a string of the form short+long+short, with the shorter string on the outside and the longer string on the inside. The strings will not be the same length, but they may be empty (length0).
+function solution(a, b){
+  if (a > b) {
+    return a + b + a;
+  } else if (a < b) {
+    return b + a + b;
+  }
+}

8b53e05521ed952731a366b230ce358a3abb88a4
Andrew KIl <kil.andrew@gmail.com>
2018-09-02 16:06:21 -0400

    solved dna to rna & lightsaber algorithms

---
+++ dna-to-rna-conversion.js
@@ -0,0 +1,15 @@
+// Deoxyribonucleic acid, DNA is the primary information storage molecule in biological systems. It is composed of four nucleic acid bases Guanine ('G'), Cytosine ('C'), Adenine ('A'), and Thymine ('T').
+//Ribonucleic acid, RNA, is the primary messenger molecule in cells. RNA differs slightly from DNA its chemical structure and contains no Thymine. In RNA Thymine is replaced by another nucleic acid Uracil ('U').
+//Create a funciton which translates a given DNA string into RNA.
+function DNAtoRNA(dna) {
+  let splitDna = dna.split('');
+  let finalArr = [];
+  for (let i = 0; i < splitDna.length; i++) {
+    if (splitDna[i] === 'T') {
+      finalArr.push('U');
+    } else {
+      finalArr.push(splitDna[i]);
+    }
+  }
+  return finalArr.join('');
+}
---
+++ how-many-lightsabers-do-you-own.js
@@ -0,0 +1,13 @@
+/*Inspired by the development team at Vooza, write the function howManyLightsabersDoYouOwn that
+accepts the name of a programmer, and
+returns the number of lightsabers owned by that person.
+The only person who owns lightsabers is Zach, by the way. He owns 18, which is an awesome number of lightsabers. Anyone else owns 0.
+No starting help here -- you'll need to know how to write a function that accepts a parameter and returns a value based on that parameter.*/
+
+function howManyLightsabersDoYouOwn(name) {
+  if (name === 'Zach') {
+    return 18;
+  } else {
+    return 0;
+    }
+}

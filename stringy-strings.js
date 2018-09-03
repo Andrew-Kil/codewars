22c4acb330da2b0ff244265ef199792296e76ea4
Andrew KIl <kil.andrew@gmail.com>
2018-09-03 09:23:02 -0400

    solved stringy strings algorithm

---
+++ stringy-strings.js
@@ -0,0 +1,17 @@
+/* write me a function stringy that takes a size and returns a string of alternating '1s' and '0s'.
+the string should start with a 1.
+a string with size 6 should return :'101010'.
+with size 4 should return : '1010'.
+with size 12 should return : '101010101010'.
+The size will always be positive and will only use whole numbers. */
+function stringy(size) {
+  let newString = '';
+  for (let i = 1; i <= size; i++) {
+    if (i % 2 === 0) {
+      newString += 0;
+    } else if (i % 2 === 1) {
+      newString += 1;
+    }
+  }
+  return newString;
+}

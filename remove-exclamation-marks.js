261561ad5eb6798c1fc259ebae159077492c2b8e
Andrew KIl <kil.andrew@gmail.com>
2018-09-04 22:57:23 -0400

    formatted the styling of the solution using prettier add-on

--- remove-exclamation-marks.js
+++ remove-exclamation-marks.js
@@ -1,12 +1,12 @@
 // Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
 
 function removeExclamationMarks(s) {
-  let arr1 = s.split('');
+  let arr1 = s.split("");
   let arr2 = [];
   for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== '!') {
+    if (arr1[i] !== "!") {
       arr2.push(arr1[i]);
     }
   }
-  return arr2.join('');
+  return arr2.join("");
 }

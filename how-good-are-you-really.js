3abdbf2b5078a1c2827d5a824b99e69690d50e1d
Andrew KIl <kil.andrew@gmail.com>
2018-09-15 20:03:05 -0400

    solved an algorithm comparing average values, realized i forgot to wrap classPoints.length + 1 in ( )

---
+++ how-good-are-you-really.js
@@ -0,0 +1,18 @@
+/* There was a test in your class and you passed it. Congratulations!
+But you're an ambitious person. You want to know if you're better than the average student in your class.
+You got an array with your colleges' points. Now calculate the average to your points!
+Return True if you're better, else False!
+Note: Your points are not included in the array of your classes points. For calculating the average point you may add your point to the given array! */
+
+function betterThanAverage(classPoints, yourPoints) {
+  let sumPoints = yourPoints;
+  for (let i = 0; i < classPoints.length; i++) {
+    sumPoints += classPoints[i];
+  }
+  let avgPoints = sumPoints / (classPoints.length + 1);
+  if (avgPoints > yourPoints) {
+    return false;
+  } if (avgPoints < yourPoints) {
+    return true;
+  }
+}

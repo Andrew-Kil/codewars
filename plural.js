6dc5fd5ad8817b64936523c8eac5e44027ecf253
Andrew KIl <kil.andrew@gmail.com>
2018-09-22 16:52:53 -0400

    initial solution was incorrect

--- plural.js
+++ plural.js
@@ -2,9 +2,5 @@
 You only need to worry about english grammar rules for this kata, where anything that isn't singular (one of something), it is plural (not one of something).
 All values will be positive integers or floats, or zero. */
 function plural(n) {
-  if (n > 0) {
-    return true;
-  } else {
-    return false;
-  }
+  return n !== 1;
 }

ea4b26a1a4b622a5387f222f67ac86137dea63a9
Andrew KIl <kil.andrew@gmail.com>
2018-10-08 22:32:46 -0400

    solve using else/if

---
+++ 8_kyu/drink-about.js
@@ -0,0 +1,18 @@
+/* Kids drink toddy.
+Teens drink coke.
+Young adults drink beer.
+Adults drink whisky.
+Make a function that receive age, and return what they drink.
+
+Rules:
+
+Children under 14 old.
+Teens under 18 old.
+Young under 21 old.
+Adults have 21 or more. */
+function peopleWithAgeDrink(old) {
+  if (old < 14) return "drink toddy";
+  else if (old < 18) return "drink coke";
+  else if (old < 21) return "drink beer";
+  else if (old >= 21) return "drink whisky";
+}

6d58ea00044050fef6f4fba3b62e62b2d600b65f
Andrew KIl <kil.andrew@gmail.com>
2018-11-05 14:36:16 -0500

    solve 8_kyu kata

---
+++ 8_kyu/thinkful-logic-drills-traffic-light.js
@@ -0,0 +1,13 @@
+// You're writing code to control your town's traffic lights. You need a function to handle each change from green, to yellow, to red, and then to green again.
+// Complete the function that takes a string as an argument representing the current state of the light and returns a string representing the state the light should change to.
+// For example, update_light('green') should return 'yellow'.
+
+const updateLight = current => {
+  if (current === "green") {
+    return "yellow";
+  } else if (current === "yellow") {
+    return "red";
+  } else if (current === "red") {
+    return "green";
+  }
+};

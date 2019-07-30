4f4b80a057679ff1dc85a5caa79b0459ec762670
Andrew KIl <kil.andrew@gmail.com>
2019-07-30 18:49:12 -0400

    solve 6 kyu kata

--- 6_kyu/dubstep.js
+++ 6_kyu/dubstep.js
@@ -1,14 +1,8 @@
 // https://www.codewars.com/kata/dubstep/train/javascript
 
 const songDecoder = song => {
-  let splitSong = song.split("WUB");
-  let splitSong2 = "";
-  for (let i = 0; i < splitSong.length; i++) {
-    if (splitSong[i] !== "") {
-      splitSong2 += splitSong[i];
-    } else if (splitSong[i] === "") {
-      continue;
-    }
-  }
-  return splitSong2.split("").join(" ");
+  return song
+    .split("WUB")
+    .filter(word => word)
+    .join(" ");
 };

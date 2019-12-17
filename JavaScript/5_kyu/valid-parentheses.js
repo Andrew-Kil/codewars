8f0eb5baa7401e24a5c82bb83b44f0bff5c9b64c
Andrew Kil <kil.andrew@gmail.com>
2019-12-16 22:56:47 -0500

    Update valid-parentheses.js

--- JavaScript/5_kyu/valid-parentheses.js
+++ JavaScript/5_kyu/valid-parentheses.js
@@ -1,5 +1,15 @@
 // https://www.codewars.com/kata/valid-parentheses/train/javascript
 
+// Write a function called that takes a string of parentheses, and determines if the order of the parentheses is valid. The function should return true if the string is valid, and false if it's invalid.
+
+// Examples
+// "()"              =>  true
+// ")(()))"          =>  false
+// "("               =>  false
+// "(())((()())())"  =>  true
+// Constraints
+// 0 <= input.length <= 100
+
 const validParentheses = p => {
   let tracker = 0;
   for (let char of p) {

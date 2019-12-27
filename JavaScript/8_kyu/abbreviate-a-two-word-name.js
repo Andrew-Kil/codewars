1737935dbc8cecb08fd3127605c77806bb71d980
Andrew KIl <kil.andrew@gmail.com>
2019-12-27 14:48:47 -0500

    update comments

--- JavaScript/8_kyu/abbreviate-a-two-word-name.js
+++ JavaScript/8_kyu/abbreviate-a-two-word-name.js
@@ -1,10 +1,14 @@
 // https://www.codewars.com/kata/abbreviate-a-two-word-name/train/javascript
 
-/* Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
-The output should be two capital letters with a dot seperating them.
-It should look like this:
-Sam Harris => S.H
-Patrick Feeney => P.F */
+// Write a function to convert a name into initials. This kata strictly takes two words with one space in between them.
+
+// The output should be two capital letters with a dot separating them.
+
+// It should look like this:
+
+// Sam Harris => S.H
+
+// Patrick Feeney => P.F
 
 const abbrevName = name => {
   return `${name

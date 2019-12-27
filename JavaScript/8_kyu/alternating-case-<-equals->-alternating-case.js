30df05a8898d091b9aebd8f70e25fe20830647e3
Andrew KIl <kil.andrew@gmail.com>
2019-12-27 14:47:35 -0500

    update comments and refactor solution

--- JavaScript/8_kyu/alternating-case-<-equals->-alternating-case.js
+++ JavaScript/8_kyu/alternating-case-<-equals->-alternating-case.js
@@ -1,13 +1,24 @@
-/* Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase.  */
+//www.codewars.com/kata/alternating-case-%3C-equals-%3E-alternating-case/train/javascript
+
+// altERnaTIng cAsE <=> ALTerNAtiNG CaSe
+// Define String.prototype.toAlternatingCase (or a similar function/method such as to_alternating_case/toAlternatingCase/ToAlternatingCase in your selected language; see the initial solution for details) such that each lowercase letter becomes uppercase and each uppercase letter becomes lowercase. For example:
+
+// "hello world".toAlternatingCase() === "HELLO WORLD"
+// "HELLO WORLD".toAlternatingCase() === "hello world"
+// "hello WORLD".toAlternatingCase() === "HELLO world"
+// "HeLLo WoRLD".toAlternatingCase() === "hEllO wOrld"
+// "12345".toAlternatingCase() === "12345" // Non-alphabetical characters are unaffected
+// "1a2b3c4d5e".toAlternatingCase() === "1A2B3C4D5E"
+// "String.prototype.toAlternatingCase".toAlternatingCase() === "sTRING.PROTOTYPE.TOaLTERNATINGcASE"
+// As usual, your function/method should be pure, i.e. it should not mutate the original string.
+
 String.prototype.toAlternatingCase = function() {
-  let splitString = this.split(""),
+  const splitString = this.split(""),
     ansArr = [];
-  for (let i = 0; i < splitString.length; i++) {
-    if (splitString[i] === splitString[i].toUpperCase()) {
-      ansArr.push(splitString[i].toLowerCase());
-    } else {
-      ansArr.push(splitString[i].toUpperCase());
-    }
-  }
+  splitString.forEach(el =>
+    el === el.toUpperCase()
+      ? ansArr.push(el.toLowerCase())
+      : ansArr.push(el.toUpperCase())
+  );
   return ansArr.join("");
 };

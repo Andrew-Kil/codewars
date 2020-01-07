3aff4ec5314fcde6343ba66bbef78641911b103b
Andrew KIl <kil.andrew@gmail.com>
2020-01-06 23:37:42 -0500

    solve 6 kyu kata

---
+++ JavaScript/6_kyu/valid-braces.js
@@ -0,0 +1,40 @@
+// https://www.codewars.com/kata/valid-braces/train/javascript
+
+// Write a function that takes a string of braces, and determines if the order of the braces is valid. It should return true if the string is valid, and false if it's invalid.
+
+// This Kata is similar to the Valid Parentheses Kata, but introduces new characters: brackets [], and curly braces {}. Thanks to @arnedag for the idea!
+
+// All input strings will be nonempty, and will only consist of parentheses, brackets and curly braces: ()[]{}.
+
+// What is considered Valid?
+// A string of braces is considered valid if all braces are matched with the correct brace.
+
+// Examples
+// "(){}[]"   =>  True
+// "([{}])"   =>  True
+// "(}"       =>  False
+// "[(])"     =>  False
+// "[({})](]" =>  False
+
+const validBraces = braces => {
+  braces = [...braces];
+  const arr = [];
+  for (let brace of braces) {
+    if (brace === "(" || brace === "{" || brace === "[") {
+      arr.push(brace);
+    } else {
+      if (arr.length === 0) return false;
+      const last = arr[arr.length - 1];
+      if (
+        (brace === ")" && last === "(") ||
+        (brace === "]" && last === "[") ||
+        (brace === "}" && last === "{")
+      ) {
+        arr.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return arr.length === 0;
+};

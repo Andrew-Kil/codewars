3c0660cb267ce7abff5d20a633741407db6e9d12
Andrew KIl <kil.andrew@gmail.com>
2020-01-16 03:36:19 -0500

    remove sql

--- SQL/8_kyu/adults-only-sql-for-beginners-number-1.sql
+++
@@ -1,8 +0,0 @@
--- https://www.codewars.com/kata/adults-only-sql-for-beginners-number-1/train/sql
-
-SELECT
-    *
-FROM
-    users
-WHERE
-   age >= 18;
--- SQL/8_kyu/sql-basics-simple-sum.sql
+++
@@ -1,4 +0,0 @@
--- https://www.codewars.com/kata/sql-basics-simple-sum/train/sql
-
-SELECT SUM(age) AS age_sum
-FROM people

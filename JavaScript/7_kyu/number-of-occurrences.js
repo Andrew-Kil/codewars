4122e55d79f01278fc97c569002fe0adf3033ab7
Andrew KIl <kil.andrew@gmail.com>
2020-02-21 23:09:41 -0500

    solve 7 kyu kata

---
+++ JavaScript/7_kyu/number-of-occurrences.js
@@ -0,0 +1,14 @@
+// https://www.codewars.com/kata/52829c5fe08baf7edc00122b/train/javascript
+
+// Write a functionthat returns the number of occurrences of an element in an array.
+
+// Examples
+// var arr = [0, 1, 2, 2, 3];
+// arr.numberOfOccurrences(0) === 1;
+// arr.numberOfOccurrences(4) === 0;
+// arr.numberOfOccurrences(2) === 2;
+// arr.numberOfOccurrences("a") === 0;
+
+Array.prototype.numberOfOccurrences = function(num) {
+  return this.filter(el => el === num).length;
+};

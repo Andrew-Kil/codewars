6acf3f93a52e0543bbd6ba8a0afdbe7bf42384a1
Andrew KIl <kil.andrew@gmail.com>
2020-03-03 09:17:33 -0500

    solve 4 kyu kata

---
+++ JavaScript/4_kyu/sudoku-solution-validator.js
@@ -0,0 +1,106 @@
+// https://www.codewars.com/kata/529bf0e9bdf7657179000008/train/javascript
+
+// Sudoku Background
+// Sudoku is a game played on a 9x9 grid. The goal of the game is to fill all cells of the grid with digits from 1 to 9, so that each column, each row, and each of the nine 3x3 sub-grids (also known as blocks) contain all of the digits from 1 to 9.
+// (More info at: http://en.wikipedia.org/wiki/Sudoku)
+
+// Sudoku Solution Validator
+// Write a function validSolution/ValidateSolution/valid_solution() that accepts a 2D array representing a Sudoku board, and returns true if it is a valid solution, or false otherwise. The cells of the sudoku board may also contain 0's, which will represent empty cells. Boards containing one or more zeroes are considered to be invalid solutions.
+
+// The board is always 9 cells by 9 cells, and every cell only contains integers from 0 to 9.
+
+// Examples
+// validSolution([
+//   [5, 3, 4, 6, 7, 8, 9, 1, 2],
+//   [6, 7, 2, 1, 9, 5, 3, 4, 8],
+//   [1, 9, 8, 3, 4, 2, 5, 6, 7],
+//   [8, 5, 9, 7, 6, 1, 4, 2, 3],
+//   [4, 2, 6, 8, 5, 3, 7, 9, 1],
+//   [7, 1, 3, 9, 2, 4, 8, 5, 6],
+//   [9, 6, 1, 5, 3, 7, 2, 8, 4],
+//   [2, 8, 7, 4, 1, 9, 6, 3, 5],
+//   [3, 4, 5, 2, 8, 6, 1, 7, 9]
+// ]); // => true
+// validSolution([
+//   [5, 3, 4, 6, 7, 8, 9, 1, 2],
+//   [6, 7, 2, 1, 9, 0, 3, 4, 8],
+//   [1, 0, 0, 3, 4, 2, 5, 6, 0],
+//   [8, 5, 9, 7, 6, 1, 0, 2, 0],
+//   [4, 2, 6, 8, 5, 3, 7, 9, 1],
+//   [7, 1, 3, 9, 2, 4, 8, 5, 6],
+//   [9, 0, 1, 5, 3, 7, 2, 1, 4],
+//   [2, 8, 7, 4, 1, 9, 6, 3, 5],
+//   [3, 0, 0, 4, 8, 1, 1, 7, 9]
+// ]); // => false
+
+const checkZeros = board => {
+  for (let i = 0; i < board.length; i++) {
+    if (board[i].includes(0)) return true;
+  }
+  return false;
+};
+
+const checkRows = board => {
+  for (let i = 0; i < board.length; i++) {
+    const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    for (let j = 0; j < board[i].length; j++) {
+      if (nums.includes(board[i][j])) {
+        nums.splice(board[i][j] - 1, 1, "x");
+      } else {
+        return false;
+      }
+    }
+  }
+  return true;
+};
+
+const checkColumns = board => {
+  let currCol = 0;
+  while (currCol < board.length) {
+    const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    for (let i = 0; i < board.length; i++) {
+      if (nums.includes(board[i][currCol])) {
+        nums.splice(board[i][currCol] - 1, 1, "x");
+      } else {
+        return false;
+      }
+    }
+    currCol++;
+  }
+  return true;
+};
+
+const checkBlocks = board => {
+  for (let i = 0; i < board.length; i += 3) {
+    for (let j = 0; j < board.length; j += 3) {
+      let block = [
+        board[i][j],
+        board[i][j + 1],
+        board[i][j + 2],
+        board[i + 1][j],
+        board[i + 1][j + 1],
+        board[i + 1][j + 2],
+        board[i + 2][j],
+        board[i + 2][j + 1],
+        board[i + 2][j + 2]
+      ];
+      const nums = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+      for (let k = 0; k < block.length; k++) {
+        if (nums.includes(block[k])) {
+          nums.splice(block[k] - 1, 1, "x");
+        } else {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+};
+
+const validSolution = board => {
+  if (checkZeros(board)) return false;
+  if (!checkRows(board)) return false;
+  if (!checkColumns(board)) return false;
+  if (!checkBlocks(board)) return false;
+  return true;
+};

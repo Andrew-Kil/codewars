32a7c3242d1e4ba36002f5060a9b7bc623e2880c
Andrew KIl <kil.andrew@gmail.com>
2020-03-07 16:18:52 -0500

    solve 7 kyu kata

---
+++ JavaScript/7_kyu/two-oldest-ages.js
@@ -0,0 +1,11 @@
+// https://www.codewars.com/kata/511f11d355fe575d2c000001/train/javascript
+
+// The two oldest ages function/method needs to be completed. It should take an array of numbers as its argument and return the two highest numbers within the array. The returned value should be an array in the format [second oldest age, oldest age].
+
+// The order of the numbers passed in could be any order. The array will always include at least 2 items.
+
+// For example:
+
+// twoOldestAges( [1, 2, 10, 8] ) // should return [8, 10]
+
+const twoOldestAges = ages => ages.sort((a, b) => a - b).slice(-2);

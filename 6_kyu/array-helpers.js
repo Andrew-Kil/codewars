cdbc826733907d397658a3c426cb1f0176d8c63d
Andrew KIl <kil.andrew@gmail.com>
2020-03-29 23:00:16 -0400

    solve 6 kyu kata

---
+++ 6_kyu/array-helpers.js
@@ -0,0 +1,47 @@
+// https://www.codewars.com/kata/525d50d2037b7acd6e000534/train/javascript
+
+// This kata is designed to test your ability to extend the functionality of built-in classes. In this case, we want you to extend the built-in Array class with the following methods: square(), cube(), average(), sum(), even() and odd().
+
+// Explanation:
+
+// square() must return a copy of the array, containing all values squared
+// cube() must return a copy of the array, containing all values cubed
+// average() must return the average of all array values; on an empty array must return NaN (note: the empty array is not tested in Ruby!)
+// sum() must return the sum of all array values
+// even() must return an array of all even numbers
+// odd() must return an array of all odd numbers
+// Note: the original array must not be changed in any case!
+
+// Example
+// var numbers = [1, 2, 3, 4, 5];
+
+// numbers.square();  // must return [1, 4, 9, 16, 25]
+// numbers.cube();    // must return [1, 8, 27, 64, 125]
+// numbers.average(); // must return 3
+// numbers.sum();     // must return 15
+// numbers.even();    // must return [2, 4]
+// numbers.odd();     // must return [1, 3, 5]
+
+Array.prototype.square = function square() {
+  return this.map(val => Math.pow(val, 2));
+};
+
+Array.prototype.cube = function cube() {
+  return this.map(val => Math.pow(val, 3));
+};
+
+Array.prototype.average = function average() {
+  return this ? this.reduce((acc, curr) => acc + curr, 0) / this.length : NaN;
+};
+
+Array.prototype.sum = function sum() {
+  return this.reduce((acc, curr) => acc + curr, 0);
+};
+
+Array.prototype.even = function even() {
+  return this.filter(val => val % 2 === 0);
+};
+
+Array.prototype.odd = function odd() {
+  return this.filter(val => val % 2 === 1);
+};

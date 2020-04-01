e1e150fe1727570d4fa31c80d8a5758454577add
Andrew KIl <kil.andrew@gmail.com>
2020-03-31 21:17:28 -0400

    solve 8 kyu kata

---
+++ 8-kyu/template-strings.js
@@ -0,0 +1,13 @@
+// https://www.codewars.com/kata/55a14f75ceda999ced000048/train/javascript
+
+// Template Strings
+// Template Strings, this kata is mainly aimed at the new JS ES6 Update introducing Template Strings
+// Task
+// Your task is to return the correct string using the Template String Feature.
+// Input
+// Two Strings, no validation is needed.
+// Output
+// You must output a string containing the two strings with the word ```' are '```
+// Reference: https://developer.mozilla.org/en/docs/Web/JavaScript/Reference/template_strings
+
+const TempleStrings = (obj, feature) => `${obj} are ${feature}`;

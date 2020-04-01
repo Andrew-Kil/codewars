c37ce12d3faab771ed3afb36d8461b8297346f08
Andrew KIl <kil.andrew@gmail.com>
2020-04-01 12:22:56 -0400

    solve beta kata

---
+++ beta-kata/thinkful-string-drills-hello-world.js
@@ -0,0 +1,10 @@
+// https://www.codewars.com/kata/5859d3325a8a0747180000c6/train/javascript
+
+// Complete the function body for this hello() function so that it takes one argument (person, a string) and returns a string saying hello to that person. The result should be formatted so that when you call the function like this:
+
+// hello "Grae"
+// you return a string that looks like this:
+
+// "Hello, Grae"
+
+const hello = name => `Hello, ${name}`;

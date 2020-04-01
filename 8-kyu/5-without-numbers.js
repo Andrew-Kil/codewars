f93b7680eed40dc3998b07d218e0766117ba1738
Andrew KIl <kil.andrew@gmail.com>
2020-03-31 20:50:51 -0400

    solve 8 kyu kata

---
+++ 8-kyu/5-without-numbers.js
@@ -0,0 +1,9 @@
+// https://www.codewars.com/kata/59441520102eaa25260000bf/train/javascript
+
+// Write a function that always returns 5
+
+// Sounds easy right? Just bear in mind that you can't use any of the following characters: 0123456789*+-/
+
+// Good luck :)
+
+const unusualFive = (arr = ["a", "b", "c", "d", "e"]) => arr.length;

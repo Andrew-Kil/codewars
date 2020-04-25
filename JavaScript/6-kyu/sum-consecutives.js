96ae19e30615a9113f54f76cc60bba7de904e2b5
Andrew KIl <kil.andrew@gmail.com>
2020-04-24 22:06:34 -0400

    create javascript and sql directories

---
+++ JavaScript/6-kyu/sum-consecutives.js
@@ -0,0 +1,43 @@
+// https://www.codewars.com/kata/55eeddff3f64c954c2000059/train/javascript
+
+// You are given a list/array which contains only integers (positive and negative). Your job is to sum only the numbers that are the same and consecutive. The result should be one list.
+
+// Extra credit if you solve it in one line. You can asume there is never an empty list/array and there will always be an integer.
+
+// Same meaning: 1 == 1
+
+// 1 != -1
+
+// #Examples:
+
+// [1,4,4,4,0,4,3,3,1] # should return [1,12,0,4,6,1]
+
+// """So as you can see sum of consecutives 1 is 1
+// sum of 3 consecutives 4 is 12
+// sum of 0... and sum of 2
+// consecutives 3 is 6 ..."""
+
+// [1,1,7,7,3] # should return [2,14,3]
+// [-5,-5,7,7,12,0] # should return [-10,14,12,0]
+
+const sumConsecutives = s => {
+  // iterate through s
+  // check curr, if current is the same as previous, store the sum
+  // for each iteration, update prev to be curr and update curr to be the next
+  // if curr is not the same as previous, push the temp sum into result array
+  // return result array
+  let result = [];
+  let curr = s[0];
+  let prev = null;
+  let sum = null;
+  for (let i = 0; i < s.length; i++) {
+    curr = s[i];
+    sum += curr;
+    if (curr === prev) sum += prev;
+    else {
+      result.push(sum);
+    }
+    prev = curr;
+  }
+  return result;
+};

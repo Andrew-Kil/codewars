a3f978cd62ac824fd6d87a60a7c4e86347c39554
Andrew KIl <kil.andrew@gmail.com>
2020-04-24 22:07:03 -0400

    solve 8 kyu kata

---
+++ SQL/8-kyu/sql-basics-simple-where-and-order-by.sql
@@ -0,0 +1,18 @@
+-- https://www.codewars.com/kata/5809508cc47d327c12000084/train/sql
+
+-- For this challenge you need to create a simple SELECT statement that will return all columns from the people table WHERE their age is over 50
+
+-- people table schema
+-- id
+-- name
+-- age
+-- You should return all people fields where their age is over 50 and order by the age descending
+
+-- NOTE: Your solution should use pure SQL. Ruby is used within the test cases to do the actual testing.
+
+-- NOTE 2: Don't end your query with a semicolon.
+
+SELECT *
+FROM people
+WHERE age > 50
+ORDER BY age DESC

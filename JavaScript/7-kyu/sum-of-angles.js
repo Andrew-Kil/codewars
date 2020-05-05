096dc6ba966fc5e2b42298cf5fc7dbe683852361
Andrew KIl <kil.andrew@gmail.com>
2020-05-05 17:44:44 -0400

    solve 7 kyu kata

---
+++ JavaScript/7-kyu/sum-of-angles.js
@@ -0,0 +1,5 @@
+// https://www.codewars.com/kata/5a03b3f6a1c9040084001765/train/javascript
+
+// Find the total sum of internal angles in an n-sided simple polygon. N will be greater than 2.
+
+const angle = (n) => (n - 2) * 180;

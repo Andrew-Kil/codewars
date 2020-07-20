5d8f97fa10a3dc64cbc77f30d43ee08f34f1d993
Andrew KIl <kil.andrew@gmail.com>
2020-07-19 21:37:27 -0400

    refactor solution

--- JavaScript/6-kyu/is-integer-array.js
+++ JavaScript/6-kyu/is-integer-array.js
@@ -9,14 +9,4 @@
 // returns true / True if array is empty.
 // returns false / False for every other input.
 
-const isIntArray = (arr) => {
-  if (!Array.isArray(arr)) return false;
-  if (
-    arr.every((el) => Number(el) === el && Number.isInteger(el)) ||
-    arr.every((el) => Number(el) === el && el % 1 !== 0) ||
-    arr.length === 0
-  ) {
-    return true;
-  }
-  return false;
-};
+const isIntArray = (a) => (!a ? false : a.every((el) => Number.isInteger(el)));

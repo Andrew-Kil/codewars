848d7cfb8edcce25d80b29349eef73978e51b47b
Andrew Kil <kil.andrew@gmail.com>
2021-08-14 18:15:47 -0400

    solve 7 kyu kata

---
+++ JavaScript/7-kyu/testing-1-2-3.js
@@ -0,0 +1,17 @@
+// Your team is writing a fancy new text editor and you've been tasked with implementing the line numbering.
+
+// Write a function which takes a list of strings and returns each line prepended by the correct number.
+
+// The numbering starts at 1. The format is n: string. Notice the colon and space in between.
+
+// Examples:
+
+// number([]) // => []
+// number(["a", "b", "c"]) // => ["1: a", "2: b", "3: c"]
+
+const number = (array) => {
+  return array.reduce((acc, cur, idx) => {
+    acc.push(`${idx + 1}: ${cur}`);
+    return acc;
+  }, []);
+};

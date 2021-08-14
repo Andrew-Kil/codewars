65992d6a18b0d708c61d89133fa6137e586ef009
Andrew Kil <kil.andrew@gmail.com>
2021-08-14 18:58:49 -0400

    solve 6 kyu kata

---
+++ JavaScript/6-kyu/difference-of-2.js
@@ -0,0 +1,29 @@
+// https://www.codewars.com/kata/5340298112fa30e786000688/train/javascript
+
+// The objective is to return all pairs of integers from a given array of integers that have a difference of 2.
+
+// The result array should be sorted in ascending order of values.
+
+// Assume there are no duplicate integers in the array. The order of the integers in the input array should not matter.
+
+// Examples
+// [1, 2, 3, 4]  should return [[1, 3], [2, 4]]
+
+// [4, 1, 2, 3]  should also return [[1, 3], [2, 4]]
+
+// [1, 23, 3, 4, 7] should return [[1, 3]]
+
+// [4, 3, 1, 5, 6] should return [[1, 3], [3, 5], [4, 6]]
+
+const twosDifference = (input) => {
+  input.sort((a, b) => a - b);
+  const result = [];
+  for (let i = 0; i < input.length; i++) {
+    for (let j = 1; j < input.length; j++) {
+      if (input[j] - input[i] === 2) {
+        result.push([input[i], input[j]]);
+      }
+    }
+  }
+  return result;
+};

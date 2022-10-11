4305ec6314e137a728f3b2fc0f7173aed83abeab
Andrew Kil <kil.andrew@gmail.com>
2022-10-10 21:30:38 -0400

    solve 6 kyu kata

---
+++ JavaScript/6-kyu/multi-tap-keypad-text-entry-on-an-old-mobile-phone.js
@@ -0,0 +1,43 @@
+// https://www.codewars.com/kata/54a2e93b22d236498400134b/train/javascript
+
+// Prior to having fancy iPhones, teenagers would wear out their thumbs sending SMS messages on candybar-shaped feature phones with 3x4 numeric keypads.
+
+// ------- ------- -------
+// |     | | ABC | | DEF |
+// |  1  | |  2  | |  3  |
+// ------- ------- -------
+// ------- ------- -------
+// | GHI | | JKL | | MNO |
+// |  4  | |  5  | |  6  |
+// ------- ------- -------
+// ------- ------- -------
+// |PQRS | | TUV | | WXYZ|
+// |  7  | |  8  | |  9  |
+// ------- ------- -------
+// ------- ------- -------
+// |     | |space| |     |
+// |  *  | |  0  | |  #  |
+// ------- ------- -------
+// Prior to the development of T9 (predictive text entry) systems, the method to type words was called "multi-tap" and involved pressing a button repeatedly to cycle through the possible values.
+
+// For example, to type a letter "R" you would press the 7 key three times (as the screen display for the current character cycles through P->Q->R->S->7). A character is "locked in" once the user presses a different key or pauses for a short period of time (thus, no extra button presses are required beyond what is needed for each letter individually). The zero key handles spaces, with one press of the key producing a space and two presses producing a zero.
+
+// In order to send the message "WHERE DO U WANT 2 MEET L8R" a teen would have to actually do 47 button presses. No wonder they abbreviated.
+
+// For this assignment, write a module that can calculate the amount of button presses required for any phrase. Punctuation can be ignored for this exercise. Likewise, you can assume the phone doesn't distinguish between upper/lowercase characters (but you should allow your module to accept input in either for convenience).
+
+// Hint: While it wouldn't take too long to hard code the amount of keypresses for all 26 letters by hand, try to avoid doing so! (Imagine you work at a phone manufacturer who might be testing out different keyboard layouts, and you want to be able to test new ones rapidly.)
+
+const one = ["a", "d", "g", "j", "m", "p", "t", "w", " ", "1"];
+const two = ["b", "e", "h", "j", "n", "q", "u", "x", "0"];
+const three = ["c", "f", "i", "l", "o", "r", "v", "y"];
+const four = ["s", "z", "2", "3", "4", "5", "6", "8"];
+const five = ["7", "9"];
+
+const buttonPresses = [one, two, three, four, five].reduce((acc, cur, idx) => {
+  cur.forEach((char) => (acc[char] = idx + 1));
+  return acc;
+}, {});
+
+const presses = (phrase) =>
+  [...phrase].reduce((acc, cur) => acc + buttonPresses[cur.toLowerCase()], 0);
